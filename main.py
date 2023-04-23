1385c775c0855a6c7d7cd1d9faf626df59a5d92d
disarmyouwitha <disarmyouwitha@gmail.com>
2023-04-22 21:12:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import asyncio
+import uvicorn
+from typing import Union
+from pathlib import Path
+from modules import shared
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Any, Dict, Optional
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+# bark imports:
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+
+# load models once:
+
+# download and load all models
+preload_models()
+
+# start fastapi for inference:
+
+# generate audio from text
+text_prompt = """
+     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+     But I also have other interests such as playing tic tac toe.
+"""
+audio_array = generate_audio(text_prompt, history_prompt="en_speaker_1")
+print(audio_array)
+
+# return audio as audio stream:
+
+write_wav("/home/nap/audio.wav", SAMPLE_RATE, audio_array)
