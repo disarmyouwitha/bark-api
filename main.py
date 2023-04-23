b52e252e47bd54301785ee2027d9e6b68a743504
disarmyouwitha <disarmyouwitha@gmail.com>
2023-04-22 21:57:02 -0500

    first commit

--- main.py
+++ main.py
@@ -20,19 +20,6 @@ from bark.generation import SAMPLE_RATE, preload_models, codec_decode, generate_
 tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
 
 
-# download and load all models
-preload_models(
-    text_use_gpu=True,
-    text_use_small=False,
-    coarse_use_gpu=True,
-    coarse_use_small=False,
-    fine_use_gpu=True,
-    fine_use_small=False,
-    codec_use_gpu=True,
-    force_reload=False
-)
-
-
 # Setup FastAPI:
 app = FastAPI()
 semaphore = asyncio.Semaphore(1)
@@ -46,37 +33,84 @@ app.add_middleware(
 )
 
 
-# [Enter your prompt and speaker here]:
-text_prompt = "You're a kid now, you're a squid now!"
-voice_name = "speaker_0" # use your custom voice name here if you have one
-
-# simple generation
-#audio_array = generate_audio(text_prompt, history_prompt=voice_name, text_temp=0.7, waveform_temp=0.7)
-
-# generation with more control
-x_semantic = generate_text_semantic(
-    text_prompt,
-    history_prompt=voice_name,
-    temp=0.7,
-    top_k=50,
-    top_p=0.95,
-)
-
-x_coarse_gen = generate_coarse(
-    x_semantic,
-    history_prompt=voice_name,
-    temp=0.7,
-    top_k=50,
-    top_p=0.95,
-)
-x_fine_gen = generate_fine(
-    x_coarse_gen,
-    history_prompt=voice_name,
-    temp=0.5,
-)
-audio_array = codec_decode(x_fine_gen)
+class GenerateRequest(BaseModel):
+    message: str
 
 
-# save audio
-filepath = "/home/nap/audio.wav"
-write_wav(filepath, SAMPLE_RATE, audio_array)
+@app.post("/generate")
+async def stream_data(req: GenerateRequest):
+    while True:
+        try:
+            # Attempt to acquire the semaphore without waiting
+            await asyncio.wait_for(semaphore.acquire(), timeout=0.1)
+            break
+        except asyncio.TimeoutError:
+            print("Server is busy")
+            await asyncio.sleep(1)
+            #raise HTTPException(status_code=503, detail="Server is busy, please try again later")
+
+    try:
+        print(req.message)
+
+        # [Enter your prompt and speaker here]:
+        text_prompt = "You're a kid now, you're a squid now!"
+        voice_name = "en_speaker_1"
+
+        # simple generation
+        #audio_array = generate_audio(text_prompt, history_prompt=voice_name, text_temp=0.7, waveform_temp=0.7)
+
+        # generation with more control
+        x_semantic = generate_text_semantic(
+            req.message,
+            history_prompt=voice_name,
+            temp=0.7,
+            top_k=50,
+            top_p=0.95,
+        )
+
+        x_coarse_gen = generate_coarse(
+            x_semantic,
+            history_prompt=voice_name,
+            temp=0.7,
+            top_k=50,
+            top_p=0.95,
+        )
+        x_fine_gen = generate_fine(
+            x_coarse_gen,
+            history_prompt=voice_name,
+            temp=0.5,
+        )
+        audio_array = codec_decode(x_fine_gen)
+
+        # save audio
+        filepath = "/home/nap/audio.wav"
+        write_wav(filepath, SAMPLE_RATE, audio_array)
+
+        return { "bark": filepath }
+
+
+
+    except Exception as e:
+        return {'response': f"Exception while processing request: {e}"}
+
+    finally:
+        semaphore.release()
+
+if __name__ == "__main__":
+    # download and load all models
+    preload_models(
+        text_use_gpu=True,
+        text_use_small=False,
+        coarse_use_gpu=True,
+        coarse_use_small=False,
+        fine_use_gpu=True,
+        fine_use_small=False,
+        codec_use_gpu=True,
+        force_reload=False
+    )
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=7862
+    )
